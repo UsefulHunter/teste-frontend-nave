8321bbff5aff42a54c0ad7fea268cb5c06a14b02
Brian Icaro D. Marques <brian.icaro.dm@gmail.com>
2020-08-24 21:52:37 -0300

    forgot to make the styles file fro the Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,67 +3,7 @@ import { navigate } from "gatsby"
 import api from "../../services/api"
 import Header from "../../components/Header"
 import ItemSingle from "../../components/Item"
-import styled from "styled-components"
-import { colors } from "../../utils/Colors"
-const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0 32px;
-`
-const TitleWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 40px;
-
-  @media (max-width: 768px) {
-    flex-flow: column nowrap;
-    margin-top: 0;
-  }
-`
-
-const Title = styled.h1`
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 40px;
-  line-height: 48px;
-  display: flex;
-  align-items: center;
-  color: ${colors.mediumBlack};
-
-  @media (max-width: 768px) {
-    font-size: 58px;
-  }
-`
-
-const Button = styled.button`
-  background-color: ${colors.mediumBlack};
-  color: ${colors.white};
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 14px;
-  line-height: 24px;
-  height: 40px;
-  padding: 8px 16px;
-
-  @media (max-width: 768px) {
-    width: 100%;
-  }
-`
-const ItemArea = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  margin-top: 32px;
-  justify-content: space-around;
-
-  @media (max-width: 768px) {
-    flex-flow: column nowrap;
-  }
-`
-
+import { HomeContainer, TitleWrapper, Title, Button, ItemArea } from "./styles"
 const Home = () => {
   const [items, setItems] = useState([])
 
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,59 @@
+import styled from "styled-components"
+import { colors } from "../../utils/Colors"
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 32px;
+`
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 40px;
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    margin-top: 0;
+  }
+`
+export const Title = styled.h1`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 48px;
+  display: flex;
+  align-items: center;
+  color: ${colors.mediumBlack};
+
+  @media (max-width: 768px) {
+    font-size: 58px;
+  }
+`
+export const Button = styled.button`
+  background-color: ${colors.mediumBlack};
+  color: ${colors.white};
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 24px;
+  height: 40px;
+  padding: 8px 16px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+export const ItemArea = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  margin-top: 32px;
+  justify-content: space-around;
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+  }
+`
